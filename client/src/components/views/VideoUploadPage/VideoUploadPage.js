6383e5d0a30802128d1931cd54eae21610fcc982
김강연 <kangyeon9525@gmail.com>
2026-03-21 02:33:44 +0900

    feat: 비디오 업로드 폼 만들기 2

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,45 @@
 import React from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import { useState } from "react";
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+  { value: 0, label: "Priavte" },
+  { value: 1, label: "Public" },
+];
+
+const CategoryOptions = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
+
 function VideoUploadPage() {
+  const [VideoTitle, setVideoTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState("Film & Animation");
+
+  const onTitleChange = (e) => {
+    setVideoTitle(e.currentTarget.value);
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.currentTarget.value);
+  };
+
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.currentTarget.value);
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -41,24 +75,36 @@ function VideoUploadPage() {
 
         <br />
         <br />
+
         <label>Title</label>
-        <Input onChange value />
-        <br />
-        <br />
-        <label>Descriptoin</label>
-        <TextArea onChange value />
+        <Input onChange={onTitleChange} value={VideoTitle} />
+
         <br />
         <br />
 
-        <select onChange>
-          <option></option>
+        <label>Description</label>
+        <TextArea onChange={onDescriptionChange} value={Description} />
+
+        <br />
+        <br />
+
+        <select onChange={onPrivateChange}>
+          {PrivateOptions.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
 
         <br />
         <br />
 
-        <select onChange>
-          <option></option>
+        <select onChange={onCategoryChange}>
+          {CategoryOptions.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
 
         <br />
