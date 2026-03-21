38c844c0f33069dbb1afefd39e66b342462a1b1e
김강연 <kangyeon9525@gmail.com>
2026-03-21 15:34:54 +0900

    feat: ffmpeg로 비디오 썸네일 생성하기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -24,7 +24,9 @@ function VideoUploadPage() {
   const [Description, setDescription] = useState("");
   const [Private, setPrivate] = useState(0);
   const [Category, setCategory] = useState("Film & Animation");
-
+  const [FilePath, setFilePath] = useState("");
+  const [Duration, setDuration] = useState("");
+  const [ThumbnailPath, setThumbnailPath] = useState("");
   const onTitleChange = (e) => {
     setVideoTitle(e.currentTarget.value);
   };
@@ -50,6 +52,22 @@ function VideoUploadPage() {
     Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
       if (response.data.success) {
         console.log(response.data);
+
+        let variable = {
+          url: response.data.url,
+          fileName: response.data.fileName,
+        };
+
+        setFilePath(response.data.url);
+
+        Axios.post("/api/video/thumbnail", variable).then((response) => {
+          if (response.data.success) {
+            setDuration(response.data.fileDuration);
+            setThumbnailPath(response.data.url);
+          } else {
+            alert("썸네일 생성에 실패 했습니다.");
+          }
+        });
       } else {
         alert("비디오 업로드를 실패했습니다.");
       }
@@ -84,9 +102,14 @@ function VideoUploadPage() {
           </Dropzone>
 
           {/* Thumbnail Zone */}
-          <div>
-            <img src alt />
-          </div>
+          {ThumbnailPath && (
+            <div>
+              <img
+                src={`http://localhost:5000/${ThumbnailPath}`}
+                alt="thumbnail"
+              />
+            </div>
+          )}
         </div>
 
         <br />
--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
+var ffmpeg = require("fluent-ffmpeg");
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -39,6 +40,50 @@ router.post("/uploadfiles", (req, res) => {
       fileName: res.req.file.filename,
     });
   });
+});
+
+router.post("/thumbnail", (req, res) => {
+  // 썸네일 생성 하고 비디오 러닝타임도 가져오기.
+
+  let filePath = "";
+  let fileDuration = "";
+
+  // 비디오 정보 가져오기
+  ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+    console.dir(metadata);
+    console.log(metadata.format.duration);
+    fileDuration = metadata.format.duration;
+  });
+
+  // 썸네일 생성
+  ffmpeg(req.body.url)
+    .on("filenames", function (filenames) {
+      console.log("Will generate" + filenames.join(", "));
+      console.log(filenames);
+
+      filePath = "uploads/thumbnails/" + filenames[0];
+    })
+    .on("end", function (filenames) {
+      console.log("Screenshots taken");
+      return res.json({
+        success: true,
+        url: filePath,
+        fileDuration: fileDuration,
+      });
+    })
+    .on("error", function (err) {
+      console.log(err);
+      return res.json({
+        success: false,
+        err,
+      });
+    })
+    .screenshot({
+      count: 3,
+      folder: "uploads/thumbnails",
+      size: "320x240",
+      fileName: "thumbnail-%b.png",
+    });
 });
 
 module.exports = router;
